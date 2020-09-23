e0b6895363b5f757d56032703abfd35aa01c3dba
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-09-23 20:22:17 +0200

    prodotto

---
+++ Prodotto.java
@@ -0,0 +1,11 @@
+import java.util.GregorianCalendar;
+
+public class Prodotto {
+
+  public Reparto reparto;
+  public String categoria, scaffale, nome, produttore, unitàMisura, responsabile, telefono;
+  public int quantità, codice;
+  public double prezzo, sconto;
+  public GregorianCalendar scadenza;
+
+}
